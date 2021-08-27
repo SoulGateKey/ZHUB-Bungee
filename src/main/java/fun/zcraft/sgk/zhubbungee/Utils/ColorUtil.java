3323499fa7296c5f13bd2b3bbbd65bd6bb5a1d27
SoulGateKey <2772023146@qq.com>
2021-08-27 18:38:32 +0800

    Initial commit

---
+++ src/main/java/fun/zcraft/sgk/zhubbungee/Utils/ColorUtil.java
@@ -0,0 +1,10 @@
+package fun.zcraft.sgk.zhubbungee.Utils;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class ColorUtil {
+
+    public static String ChangeColor(String text) {
+        return ChatColor.translateAlternateColorCodes('&',text);
+    }
+}
--- src/main/java/fun/zcraft/sgk/zhubbungee/ZHUBBungee.java
+++ src/main/java/fun/zcraft/sgk/zhubbungee/ZHUBBungee.java
@@ -1,20 +1,22 @@
 package fun.zcraft.sgk.zhubbungee;
 
 import fun.zcraft.sgk.zhubbungee.Commands.reloadCommand;
+import fun.zcraft.sgk.zhubbungee.Stats.Metrics;
+import fun.zcraft.sgk.zhubbungee.Utils.ColorUtil;
+import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
-
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.*;
 import java.nio.file.Files;
+
 
 public final class ZHUBBungee extends Plugin {
     public static ZHUBBungee instance;
     public Configuration config;
-
+    CommandSender sender;
     @Override
     public void onEnable() {
         instance = this;
@@ -22,6 +24,9 @@ public final class ZHUBBungee extends Plugin {
         saveDefaultConfig();
         reloadConfig();
         getLogger().info("ZHUB-Bungee has enabled successfully.");
+        int pluginId = 12613;
+        Metrics metrics = new Metrics(this, pluginId);
+        metrics.addCustomChart(new Metrics.SimplePie("chart_id", () -> "My value"));
         getLogger().info("The main hub server is "+ getConfig().getString("Hub" , "Lobby"));
     }
     @Override
@@ -50,8 +55,9 @@ public final class ZHUBBungee extends Plugin {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        getLogger().info("ZHUB-Bungee has reloaded successfully.%n");
-        getLogger().info("The Hub server is " + getConfig().getString("Hub"));
+        String h = getConfig().getString("Hub");
+        sender.sendMessage("ZHUB-Bungee config.yml has reloaded successfully.");
+        sender.sendMessage("The Hub server now is §a" + h);
     }
     public void saveConfig() {
         File file = new File(getDataFolder(),"config.yml");
